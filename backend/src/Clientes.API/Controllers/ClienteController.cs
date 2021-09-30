ce4ac144d2988ac65ac4e76a5081e4b3037f47df
Alisson Youssf <youssfbr@gmail.com>
2021-09-30 20:23:43 -0300

    Client mock

--- backend/src/Clientes.API/Controllers/ClienteController.cs
+++ backend/src/Clientes.API/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Clientes.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,17 +12,29 @@ namespace Clientes.API.Controllers
         public ClienteController() { }       
 
         [HttpGet]
-        public Cliente Get()
+        public IEnumerable<Cliente> Get()
         {
-            return new Cliente()
+            return new Cliente[] 
             {
-                Id = 1,
-                Nome = "Alisson Youssf",
-                Email = "youssfbr@gmail.com",
-                Telefone = "+55 (85) 9.8705.9184",
-                DataCadastro = DateTime.Now,
-                CPF = "12345567"
-            };
+                new Cliente() 
+                {
+                    Id = 1,
+                    Nome = "Alisson Youssf",
+                    Email = "youssfbr@gmail.com",
+                    Telefone = "+55 (85) 9.8705.9184",
+                    DataCadastro = DateTime.Now,
+                    CPF = "12345567"
+                },
+                new Cliente() 
+                {
+                    Id = 2,
+                    Nome = "Link da Silva Sauro",
+                    Email = "linlin@gmail.com",
+                    Telefone = "+55 (85) 9.8128-6510",
+                    DataCadastro = DateTime.Now,
+                    CPF = "12345563"
+                }
+            };            
         }
     }
 }
